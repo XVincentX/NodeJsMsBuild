3dec20f65d7256574c1efd746a831556b0cb4583
Vincenzo Chianese <vincenz.chianese@yahoo.it>
2014-07-23 21:18:14 +0200

    Remove from project and not from file system [ci skip]

--- nugetPackage/tools/Install.ps1
+++ nugetPackage/tools/Install.ps1
@@ -13,11 +13,11 @@ Function Test-CommandExists
 if (Test-CommandExists "grunt" -eq $false -or Test-CommandExists "./.bin/grunt" -eq $false -or Test-CommandExists "./node_modules/.bin/grunt" -eq $false)
 {
   Write-Host "Unable to find grunt; Gruntfile.js removed from project. You can find it anyway showing all items"
-  $project.ProjectItems.Item('Gruntfile.js').Delete()
+  $project.ProjectItems.Item('Gruntfile.js').Remove()
 }
 
 if (Test-CommandExists "gulp" -eq $false -or Test-CommandExists "./.bin/gulp" -eq $false -or Test-CommandExists "./node_modules/.bin/gulp" -eq $false)
 {
   Write-Host "Unable to find gulp; gulpfile.js removed from project. You can find it anyway showing all items"
-  $project.ProjectItems.Item('gulpfile.js').Delete()
+  $project.ProjectItems.Item('gulpfile.js').Remove()
 }
