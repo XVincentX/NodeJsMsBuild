efc471029c08761ceabf66f6c577d1a0a2076746
Vincenzo Chianese <v.chianese@nettunosolutions.com>
2014-07-24 16:11:46 +0200

    Fixed condition for project items removing

--- nugetPackage/tools/Install.ps1
+++ nugetPackage/tools/Install.ps1
@@ -10,13 +10,13 @@ Function Test-CommandExists
  Finally {$ErrorActionPreference=$oldPreference}
 }
 
-if (Test-CommandExists "grunt" -eq $false -or Test-CommandExists "./.bin/grunt" -eq $false -or Test-CommandExists "./node_modules/.bin/grunt" -eq $false)
+if (Test-CommandExists "grunt" -eq $false -and Test-CommandExists "./.bin/grunt" -eq $false -and Test-CommandExists "./node_modules/.bin/grunt" -eq $false)
 {
   Write-Host "Unable to find grunt; Gruntfile.js removed from project. You can find it anyway showing all items"
   $project.ProjectItems.Item('Gruntfile.js').Remove()
 }
 
-if (Test-CommandExists "gulp" -eq $false -or Test-CommandExists "./.bin/gulp" -eq $false -or Test-CommandExists "./node_modules/.bin/gulp" -eq $false)
+if (Test-CommandExists "gulp" -eq $false -and Test-CommandExists "./.bin/gulp" -eq $false -and Test-CommandExists "./node_modules/.bin/gulp" -eq $false)
 {
   Write-Host "Unable to find gulp; gulpfile.js removed from project. You can find it anyway showing all items"
   $project.ProjectItems.Item('gulpfile.js').Remove()
